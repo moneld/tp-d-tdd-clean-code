3df88b39f3857e877ada823d3565f69365ed2f9f
Monel.D <monelyehoun@gmail.com>
2023-01-04 01:24:15 +0100

    Ajout des fonctions

--- src/main/java/fr/esgi/cleancode/service/DrivingLicenceFinderService.java
+++ src/main/java/fr/esgi/cleancode/service/DrivingLicenceFinderService.java
@@ -1,6 +1,8 @@
 package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.exception.InvalidDriverSocialSecurityNumberException;
+import fr.esgi.cleancode.exception.ResourceNotFoundException;
 import fr.esgi.cleancode.model.DrivingLicence;
 import lombok.RequiredArgsConstructor;
 
@@ -11,15 +13,47 @@ import java.util.UUID;
 public class DrivingLicenceFinderService {
 
     private final InMemoryDatabase database;
+    private final DrivingLicenceIdGenerationService drivingLicenceIdGenerationService;
 
     public Optional<DrivingLicence> findById(UUID drivingLicenceId) {
         return database.findById(drivingLicenceId);
     }
 
-    public void addDrivingLicence(String azerty741258963) {
+    public void addDrivingLicence(String socialSecurityNumber) {
+
+        if (socialSecurityNumber.matches("^\\d{15}$")) {
+
+            UUID drivingId = drivingLicenceIdGenerationService.generateNewDrivingLicenceId();
+            DrivingLicence drivingLicence =DrivingLicence.builder()
+                    .id(drivingId)
+                    .driverSocialSecurityNumber(socialSecurityNumber)
+                    .build();
+
+            database.save(drivingId, drivingLicence);
+
+        } else{
+            throw new InvalidDriverSocialSecurityNumberException("Invalid social security number");
+        }
+
     }
 
-    public Optional<DrivingLicence> removePointOnDrivingLicence(UUID id, int i) {
-        return null;
+    public Optional<DrivingLicence> removePointOnDrivingLicence(UUID drivingId, int point) {
+        Optional<DrivingLicence> optionalDrivingLicence = this.findById(drivingId);
+
+        if (optionalDrivingLicence.isEmpty()){
+            throw new ResourceNotFoundException("Driving licence not found");
+        }
+        int newPoint = 0;
+
+        if (optionalDrivingLicence.get().getAvailablePoints() > 0 || optionalDrivingLicence.get().getAvailablePoints() - point > 0){
+            newPoint = optionalDrivingLicence.get().getAvailablePoints() - point;
+        }
+        DrivingLicence drivingLicence = DrivingLicence.builder()
+                .id(optionalDrivingLicence.get().getId())
+                .driverSocialSecurityNumber(optionalDrivingLicence.get().getDriverSocialSecurityNumber())
+                .availablePoints(newPoint)
+                .build();
+
+        return Optional.ofNullable(database.save(drivingId,drivingLicence));
     }
 }
